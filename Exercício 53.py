ee4a595a9d312d2e0c3ab578a9e77e2fa3568d45
Gisele Correia <giselehmcorreia@gmail.com>
2026-03-30 16:49:32 -0300

    Update Exercício 53.py

--- Exercício 53.py
+++ Exercício 53.py
@@ -1,3 +1,10 @@
-frase=str(input('Digite uma frase:')).upper()
-inverso=frase[::-1]
-print(f'O inverso de {frase} é {inverso}')   
+frase=str(input('Digite uma frase:')).upper().strip()
+palavra=frase.split()
+junto=''.join(palavra)
+inverso=''
+for letra in range(len(junto)-1, -1, -1):
+    inverso+=junto[letra]
+if inverso == junto:
+    print("É um palíndromo")
+else:
+    print('Não é um palíndromo.')
