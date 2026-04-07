1472f002d46eb9e32c35644d62da9bfa667e43ce
Gisele Correia <giselehmcorreia@gmail.com>
2026-04-07 20:20:37 -0300

    Update final

--- Exercício 56.py
+++ Exercício 56.py
@@ -1,15 +1,26 @@
-soma=0
+somaidade = 0
+mediaidade=0
+maioridadehomem=0
+nomevelho=''
+totmulher20=0
 
-for c in range(1,5):
+for p in range(1, 5):
+    print(f'----{p}ª PESSOA----')
+    nome = str(input(f'Nome: ')).strip().upper()
+    idade = int(input(f'Idade: '))
+    sexo = str(input(f'Sexo [M/F]: ')).strip().upper()
+    somaidade += idade
+    if p==1 and sexo in 'Mm':
+        maioridadehomem=idade
+        nomevelho=nome
+    if sexo in 'Mm' and idade>maioridadehomem:
+        maioridadehomem=idade
+        nomevelho=nome
+    if sexo in 'Ff' and idade<20:
+        totmulher20+=1
 
-    nome=str(input(f'Escreva o nome da {c}ª pessoa:')).strip().upper()
 
-    idade=int(input(f'Escreva a idade da {c}ª pessoa:'))
-
-    sexo=str(input(f'Escreva o sexo da {c}ª pessoa:')).strip().upper()
-
-    soma+=idade
-
-media=(soma/4)
-
-print(f'A média das idade é {media}')
+mediaidade = (somaidade / 4)
+print(f'A média das idades é {mediaidade}')
+print(f'O homem mais velho tem {idade} anos e se chama {nome}')
+print(f'Ao todo tem {totmulher20} mulher(es) maior(s) de 20 anos')
